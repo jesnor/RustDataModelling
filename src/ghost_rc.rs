3da143491d41a5a7b356b89abd51bb014d8d923f
Jesper Nordenberg <jesper@nnl.se>
2021-05-05 02:10:44 +0200

    Changed lifetime name

--- src/ghost_rc.rs
+++ src/ghost_rc.rs
@@ -5,63 +5,67 @@ use std::{
 
 use ghost_cell::{GhostCell, GhostToken};
 
-struct Ref<'t, T>(Rc<GhostCell<'t, T>>);
+struct Ref<'brand, T>(Rc<GhostCell<'brand, T>>);
 
-impl<'t, T> Ref<'t, T> {
+impl<'brand, T> Ref<'brand, T> {
     fn new(v: T) -> Self {
         Self(Rc::new(GhostCell::new(v)))
     }
 
-    fn weak(&self) -> Weak<GhostCell<'t, T>> {
+    fn weak(&self) -> Weak<GhostCell<'brand, T>> {
         Rc::downgrade(&self.0)
     }
 }
 
-impl<'t, T> Clone for Ref<'t, T> {
+impl<'brand, T> Clone for Ref<'brand, T> {
     fn clone(&self) -> Self {
         Ref(self.0.clone())
     }
 }
 
-impl<'t, T> Deref for Ref<'t, T> {
-    type Target = GhostCell<'t, T>;
+impl<'brand, T> Deref for Ref<'brand, T> {
+    type Target = GhostCell<'brand, T>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-type WRef<'t, T> = Weak<GhostCell<'t, T>>;
+type WRef<'brand, T> = Weak<GhostCell<'brand, T>>;
 
-struct Player<'t> {
-    game: GameWRef<'t>,
+struct Player<'brand> {
+    game: GameWRef<'brand>,
     name: String,
     health: i32,
-    friends: Vec<PlayerWRef<'t>>,
+    friends: Vec<PlayerWRef<'brand>>,
 }
 
-type PlayerRef<'t> = Ref<'t, Player<'t>>;
-type PlayerWRef<'t> = WRef<'t, Player<'t>>;
+type PlayerRef<'brand> = Ref<'brand, Player<'brand>>;
+type PlayerWRef<'brand> = WRef<'brand, Player<'brand>>;
 
-fn make_friends<'t>(token: &mut GhostToken<'t>, player1: &PlayerRef<'t>, player2: &PlayerRef<'t>) {
+fn make_friends<'brand>(
+    token: &mut GhostToken<'brand>,
+    player1: &PlayerRef<'brand>,
+    player2: &PlayerRef<'brand>,
+) {
     player1.borrow_mut(token).friends.push(player2.weak());
     player2.borrow_mut(token).friends.push(player1.weak());
 }
 
 #[derive(Default)]
-struct Game<'t> {
-    players: Vec<PlayerRef<'t>>,
+struct Game<'brand> {
+    players: Vec<PlayerRef<'brand>>,
 }
 
-type GameRef<'t> = Ref<'t, Game<'t>>;
-type GameWRef<'t> = WRef<'t, Game<'t>>;
+type GameRef<'brand> = Ref<'brand, Game<'brand>>;
+type GameWRef<'brand> = WRef<'brand, Game<'brand>>;
 
-fn create_player<'t>(
-    token: &mut GhostToken<'t>,
-    game: &GameRef<'t>,
+fn create_player<'brand>(
+    token: &mut GhostToken<'brand>,
+    game: &GameRef<'brand>,
     name: &str,
     health: i32,
-) -> PlayerRef<'t> {
+) -> PlayerRef<'brand> {
     let p = Ref::new(Player {
         game: game.weak(),
         name: name.into(),
